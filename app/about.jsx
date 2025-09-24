0859dac5d69831448bedb366800f40e4427ceb09
CarlesWebDev <charlesmarvin4321@gmail.com>
2025-09-24 14:47:17 +0700

    finish add tailwind in react native

--- app/about.jsx
+++ app/about.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 const about = () => {
   return (
     <View>
-      <Text>about</Text>
+      <Text className='font-bold'>about</Text>
     </View>
   )
 }
--- app/index.jsx
+++ app/index.jsx
@@ -1,11 +1,12 @@
 import { Link } from 'expo-router'
 import { StyleSheet, Text, View } from 'react-native'
+import "../global.css"
 
 
 const Home = () => {
   return (
     <View>
-      <Text style={{ marginBottom: 20 }}>Home</Text>
+      <Text className='text-red-500 text-center'>Home</Text>
       <Link href="/about">Go to About</Link>
     </View>
   )
