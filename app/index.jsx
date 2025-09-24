2bdfab9553f20bd058feb7a391f6fc433df431a2
CarlesWebDev <charlesmarvin4321@gmail.com>
2025-09-24 16:34:14 +0700

    Simple component: View, ,Text, Image

--- app/index.jsx
+++ app/index.jsx
@@ -1,17 +1,19 @@
 import { Link } from 'expo-router'
-import { StyleSheet, Text, View } from 'react-native'
+import { Text, View, Image } from 'react-native'
 import "../global.css"
-
+import GAMBAR2 from "../assets/icon.png"
 
 const Home = () => {
   return (
-    <View>
-      <Text className='text-red-500 text-center'>Home</Text>
-      <Link href="/about">Go to About</Link>
+    <View className='flex flex-col'>
+      <Text className='text-xl font-extrabold text-center mt-10 text-wrap px-8'>
+        My First React Native App with NativeWind 
+      </Text>
+      <Text className='font-semibold shadow-lg bg-blue-400 px-6 p-2 rounded-md mt-10 mx-auto'>Hallo Brok</Text>
+
+      <Image source={GAMBAR2}  className='w-20 h-20 mx-auto mt-10'/>
     </View>
   )
 }
 
 export default Home
-
-const styles = StyleSheet.create({})
