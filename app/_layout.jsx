0efe58a3ed749068e9d3bbad9f2966899d0bf106
CarlesWebDev <charlesmarvin4321@gmail.com>
2025-09-25 10:46:06 +0700

    Themes Light mode and Light

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,14 +1,26 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, useColorScheme, View } from 'react-native'
 import React from 'react'
 import { Stack } from 'expo-router'
+import "../global.css"
+import {Colors} from '../constants/color'
+import { StatusBar } from 'expo-status-bar'
 
 const RootLayout = () => {
+
+  const colorScheme = useColorScheme()
+  const theme = Colors[colorScheme] ?? Colors.light
+
+
+
   return (
-    <Stack screenOptions={{ headerStyle: { backgroundColor: '#ADD8E6' }, headerTintColor: '#fff', headerTitleStyle: { fontWeight: 'bold' } }}>
+    <>
+    <StatusBar value="auto" />
+    <Stack screenOptions={{ headerStyle: { backgroundColor: theme.navBackground }, headerTintColor: theme.title, headerTitleStyle: { fontWeight: 'bold' } }}>
       <Stack.Screen name='index' options={{ title: 'home' }} />
       <Stack.Screen name='about' options={{ title: 'Tentang' }} />
       <Stack.Screen name='contact' options={{ title: 'Kontak', headerShown: false }} />
-    </Stack>
+      </Stack>
+    </>
   )
 }
 export default RootLayout
--- app/about.jsx
+++ app/about.jsx
@@ -1,22 +1,26 @@
-import { View, Text, Image,} from 'react-native'
+import { View, Text, Image, useColorScheme,} from 'react-native'
 import React from 'react'
 import { Link } from 'expo-router'
 import GAMABAR1 from "../assets/img/image1.png"
 import GAMABAR2 from "../assets/img/image2.png"
 import GAMABAR3 from "../assets/img/image.png"
-
+import {Colors} from '../constants/color'
 const About = () => {
+
+  const colorScheme = useColorScheme()
+  const theme = Colors[colorScheme] ?? Colors.light
+  
   return (
-    <View className=' flex flex-col'>
-      <Text className='font-bold mt-5 px-6 text-wrap text-2xl'>
+    <View className='flex flex-col' style={{backgroundColor: theme.background, flex: 1}} >
+      <Text className='px-6 mt-5 text-2xl font-bold text-wrap'>
         My Name Lumba Lumba Jawa and i Web Devloper
       </Text>
           
-     <Text className='flex flex-col mt-10 px-6 font-semibold'>Keahlian ku:</Text>
+     <Text className='flex flex-col px-6 mt-10 font-semibold'>Keahlian ku:</Text>
 
 
-    <View className='flex-row flex-wrap px-6 space-x-2 gap-3 mt-5'>
-        <Image source={GAMABAR1} className='w-20 h-20' />
+    <View className='flex-row flex-wrap gap-3 px-6 mt-5 space-x-2'>
+        <Image source={GAMABAR1} className='w-20 h-20' style={{ backgroundColor: theme.primary }} />
         <Image source={GAMABAR2} className='w-20 h-20' />
         <Image source={GAMABAR3} className='w-20 h-20' />
         <Image source={GAMABAR1} className='w-20 h-20' />
@@ -32,7 +36,7 @@ const About = () => {
     
       <Link 
         href="/" 
-        className='font-semibold shadow-lg bg-blue-400 p-2 rounded-md mt-10 mx-auto text-center'
+        className='p-2 mx-auto mt-10 font-semibold text-center bg-blue-400 rounded-md shadow-lg'
       >
         Kembali Ke Home
       </Link>     
